a9bc12a855709f39ef6c7ce1f5d2bf9c6bb00e94
lihe <heng.li@sinosun.com.cn>
2020-11-03 18:10:46 +0800

    提交java代码

---
+++ java_proj/java_study/src/main/java/com/exercise/io/Demo00InputStreamReadChar.java
@@ -0,0 +1,27 @@
+package com.exercise.io;
+
+
+/*
+ *字节流读取字符流会乱码
+ * 读取然后写入没有问题，不能取出来
+ * */
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Demo00InputStreamReadChar {
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream fis = new FileInputStream("conf/d.txt");
+        FileOutputStream fos = new FileOutputStream("conf/e.txt");
+        int len = 0;
+        while((len=fis.read()) != -1){
+//            System.out.println(len);  //读取的是字节码,取不出来。 取出来显示就有问题。 但是可以读取出来直接写进去
+            fos.write(len);
+        }
+        fos.close();
+        fis.close();
+    }
+
+}
---
+++ java_proj/java_study/src/test/java/com/exercise/io/FileTest.java
@@ -0,0 +1,10 @@
+package com.exercise.io;
+
+import org.testng.annotations.Test;
+
+public class FileTest {
+    @Test
+    public void test_add(){
+        System.out.println("aaa");
+    }
+}
