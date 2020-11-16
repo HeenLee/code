a9b67e8b2192c8ea94a686dbe827adec9ec01619
lihe <heng.li@sinosun.com.cn>
2020-11-16 09:22:38 +0800

    提交多态

--- java_proj/java_pro_maven/src/main/java/com/exercise/duotai/Computer.java
+++ java_proj/java_pro_maven/src/main/java/com/exercise/duotai/Computer.java
@@ -1,5 +1,8 @@
 package com.exercise.duotai;
 
+
+import java.security.Key;
+
 public class Computer {
     public void powerOn(){
         System.out.println("笔记本开机");
@@ -9,9 +12,18 @@ public class Computer {
         System.out.println("笔记本关机");
     }
 
-    //使用USB设备的方法
+    //使用USB设备的方法。实现USB接口的类或者子类可以直接传入参数。但是
+    //如果是子类特有的方法，需要向下转型
     public void usrDevice(USB usb){
         usb.open();
+        if(usb instanceof Mouse){
+            Mouse mouseUsb = (Mouse) usb;
+            mouseUsb.click();
+        }
+        if(usb instanceof Keyboard){
+            Keyboard keyboardUsb = (Keyboard) usb;
+            keyboardUsb.type();
+        }
         usb.close();
     }
 }
