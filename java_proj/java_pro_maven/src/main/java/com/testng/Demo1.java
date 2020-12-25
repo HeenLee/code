530627f9400f444ffbaf9386e489ff6875347d82
lihe <heng.li@sinosun.com.cn>
2020-12-25 18:24:15 +0800

    testng

---
+++ java_proj/java_pro_maven/src/main/java/com/testng/Demo1.java
@@ -0,0 +1,11 @@
+package com.testng;
+
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+    @Test
+    public void test1(){
+        System.out.println("de");
+    }
+}
---
+++ java_proj/java_pro_maven/src/main/java/com/testng/Demo2.java
@@ -0,0 +1,11 @@
+package com.testng;
+
+
+import org.testng.annotations.Test;
+
+public class Demo2 {
+    @Test
+    public void test2() {
+        System.out.println("test2");
+    }
+}
