3f51fb66fde5d077436f7f2df59281bc95452d7e
lihe <heng.li@sinosun.com.cn>
2021-02-25 18:36:00 +0800

    mybatis

---
+++ java_proj/mybatis/hellomybatis1/src/main/java/com/sino/dao/StudentDao.java
@@ -0,0 +1,14 @@
+package com.sino.dao;
+
+import com.sino.domain.Student;
+
+import java.util.List;
+
+//接口操作Student表
+public interface StudentDao {
+    //查询student表中的所有数据
+    List<Student> selectStudents();
+
+
+
+}
